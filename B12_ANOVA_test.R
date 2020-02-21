18db7480cd94bf882cf32d84a8c2ca08cc4b2b8c
Regina Lionheart <regina@lionhe.art>
2020-02-21 15:49:31 -0800

    Apply ANOVA and Tukey Significance over all compounds.

--- B12_ANOVA_test.R
+++ B12_ANOVA_test.R
@@ -13,7 +13,27 @@ BMISd.normed <- WBMISd.normed %>%
     names_to = "Mass.Feature",
     values_to = "Area.BMISd.Normd") %>%
   select(Mass.Feature, Replicate.Name, Area.BMISd.Normd)
+#############################################################################
+WBMISd <- BMISd %>%
+  spread(key = "Replicate.Name", value = "Adjusted.Area")
+WBMISd <- WBMISd[complete.cases(WBMISd), ]
+mySamps <- colnames(WBMISd)
 
+AnovaB12 <- BMISd.normed %>%
+  filter(str_detect(Replicate.Name, "WBT|IL2noBT|Control")) %>%
+  separate(Replicate.Name, into = c("one", "two", "SampID", "four"), remove = FALSE) %>%
+  select(Mass.Feature, Replicate.Name, SampID, Area.BMISd.Normd) %>%
+  # group_by(Mass.Feature, SampID) %>%
+  # mutate(Average.Adjusted.Area = mean(Adjusted.Area, na.rm = TRUE)) %>%
+  # select(Mass.Feature, SampID, Average.Adjusted.Area) %>%
+  select(Mass.Feature, Replicate.Name, SampID, Area.BMISd.Normd) %>%
+  unique()
+AnovaB12 <- AnovaB12[complete.cases(AnovaB12), ]
+
+# WAnovaB12 <- AnovaB12 %>%
+#   pivot_wider(names_from = Mass.Feature,
+#               values_from = Adjusted.Area)
+###############################################################################
 
 # Combine all data and rearrange
 full.BMISd <- BMISd.normed %>%
@@ -34,38 +54,57 @@ glimpse(AnovaB12) #Use for the future
 levels(AnovaB12$SampID)
 
 # Graph normalized areas for reference
-ggplot(AnovaB12, aes(x = SampID, y = Area.BMISd.Normd, fill = SampID)) +
+Norm.Areas <- ggplot(AnovaB12, aes(x = SampID, y = Area.BMISd.Normd, fill = SampID)) +
   geom_boxplot() +
   facet_wrap(~Mass.Feature) +
   theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5)) +
   geom_jitter(shape = 15,
               color = "steelblue",
               position = position_jitter(0.21))
+#Norm.Areas
 
-# Split and apply ANOVA function, result is a named list:
+# Apply ANOVA to dataframe and check significance
 AnovaList <- lapply(split(AnovaB12, AnovaB12$Mass.Feature), function(i) {
-  anova(lm(Area.BMISd.Normd ~ SampID, data = i))
+  aov(lm(Area.BMISd.Normd ~ SampID, data = i))
 })
 
-# Turn list to dataframe
-AnovaDF <- do.call(rbind, lapply(AnovaList, function(x) {temp <- unlist(x)}))
-AnovaDF <- as.data.frame(AnovaDF)
-colnames(AnovaDF)[9] <- "Pvalue"
+AnovaDF <- as.data.frame(do.call(rbind, lapply(AnovaList, function(x) {temp <- unlist(x)})))
+colnames(AnovaDF)[9] <- "AnovaP"
 
 AnovaDF <- AnovaDF %>%
   rownames_to_column(var = "Mass.Feature") %>%
-  select(Mass.Feature, Pvalue) %>%
-  mutate(Significant = ifelse(Pvalue < 0.1, TRUE, FALSE))
+  mutate(AnovaSig = ifelse(AnovaP < 0.1, TRUE, FALSE)) %>%
+  select(Mass.Feature, AnovaP, AnovaSig) %>%
+  arrange(Mass.Feature)
+
+
+# Apply Tukey HSD to AnovaList and check significance
+TukeyList <- lapply(AnovaList, function(x) TukeyHSD(x))
+TukeyDF <- as.data.frame(do.call(rbind, lapply(TukeyList, function(x) {temp <- unlist(x)}))) %>%
+  select(SampID10:12) %>%
+  rownames_to_column("Mass.Feature") %>%
+  rename(noB12vControl = SampID10,
+         WB12vControl = SampID11,
+         WB12vnoB12 = SampID12) %>%
+  mutate(noB12vControl_Sig = ifelse(noB12vControl < 0.1, TRUE, FALSE),
+          WB12vControl_Sig = ifelse(WB12vControl < 0.1, TRUE, FALSE),
+          WB12vnoB12_Sig = ifelse(WB12vnoB12 < 0.1, TRUE, FALSE)) %>%
+  #mutate(WvnoB12_FC = log2(rowMeans(TukeyDF[, myTreat1]) / rowMeans(WBMISd[, myTreat2])))
+  arrange(Mass.Feature)
+
 
 # Join with original data to plot
 toPlot <- full.BMISd %>%
   left_join(AnovaDF) %>%
   group_by(Mass.Feature, SampID) %>%
   mutate(AveSmp = mean(Area.BMISd.Normd, na.rm = TRUE)) %>%
-  select(Mass.Feature, SampID, AveSmp, Pvalue, Significant) %>%
-  unique()
+  select(Mass.Feature, SampID, AveSmp, AnovaSig) %>%
+  unique() %>%
+  left_join(TukeyDF) %>%
+  arrange(Mass.Feature)
 
-a <- ggplot(toPlot, aes(x = AveSmp, y = Pvalue, fill = Significant,
+
+a <- ggplot(toPlot, aes(x = AveSmp, y = AnovaP, fill = AnovaSig,
                           label = Mass.Feature)) +
   geom_point(size = 3, shape = 21, stroke=0) +  
   scale_fill_manual(values = c("grey", "royalblue4")) +
@@ -77,23 +116,8 @@ a <- ggplot(toPlot, aes(x = AveSmp, y = Pvalue, fill = Significant,
         axis.title.x=element_text(size=9),
         axis.text=element_text(size=9)) +
   labs(x="Average peak size") +
-  labs(x="Average peak size", y=expression(paste(Log[2], "T0/DSW", sep = ""))) +
+  labs(x="Average peak size", y="AnovaP") +
   theme(legend.position="right") 
   #geom_text(data = toPlot)
 a
-
-
-
-test_anova_one_way <- aov(Area.BMISd.Normd~SampID, data = test_df)
-summary(test_anova_one_way)
-summary(test_anova_one_way)[[1]][["Pr(>F)"]]
-test <- TukeyHSD(test_anova_one_way)
-
-# two way isn't useful because mass feature isn't grouping?
-# test_anova_two_way <- aov(Area.BMISd.Normd~SampID + Mass.Feature, data = test_df)
-# summary(test_anova_two_way)
-
-
-
-
 
