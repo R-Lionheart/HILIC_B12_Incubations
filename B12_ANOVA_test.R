efe0eaca84b239d4cc24ae4096918210a1c61200
Regina Lionheart <regina@lionhe.art>
2020-02-21 12:34:52 -0800

    Clean up ANOVA script, add first draft of graph w/o Tukey.

--- B12_ANOVA_test.R
+++ B12_ANOVA_test.R
@@ -1,84 +1,93 @@
 library(tidyverse)
 options(scipen = 999)
 
-####
-BMISd <- read.csv("data_processed/IsoLagran1_0.2_notnormed.csv", stringsAsFactors = FALSE) %>%
+# Import required files
+BMISd <- read.csv("data_processed/IsoLagran2_5_notnormed.csv", stringsAsFactors = FALSE) %>%
   select(Mass.Feature:Adjusted.Area)
-WBMISd.normed <- read.csv("data_processed/IsoLagran2_5_normed.csv", stringsAsFactors = FALSE)
+WBMISd.normed <- read.csv("data_processed/IsoLagran2_5_normed.csv", stringsAsFactors = FALSE, check.names = FALSE)
+colnames(WBMISd.normed)[1] <- "Replicate.Name"
+
+# Change all sets to long format
 BMISd.normed <- WBMISd.normed %>%
-  pivot_longer(-X,
-               names_to = "Mass.Feature",
-               values_to = "Area.BMISd.Normd") %>%
-  rename(Replicate.Name = X) %>%
+  pivot_longer(-Replicate.Name,
+    names_to = "Mass.Feature",
+    values_to = "Area.BMISd.Normd") %>%
   select(Mass.Feature, Replicate.Name, Area.BMISd.Normd)
 
 
-WBMISd <- BMISd %>%
-  spread(key = "Replicate.Name", value = "Adjusted.Area")
-WBMISd <- WBMISd[complete.cases(WBMISd), ]
-mySamps <- colnames(WBMISd)
-
-AnovaB12 <- BMISd.normed %>%
-  filter(str_detect(Replicate.Name, "WBT|IL2noBT|Control")) %>%
+# Combine all data and rearrange
+full.BMISd <- BMISd.normed %>%
+  left_join(BMISd) %>%
+  filter(str_detect(Replicate.Name, "WBT|IL2noBT|IL2Control")) %>%
   separate(Replicate.Name, into = c("one", "two", "SampID", "four"), remove = FALSE) %>%
-  select(Mass.Feature, Replicate.Name, SampID, Area.BMISd.Normd) %>%
-  # group_by(Mass.Feature, SampID) %>%
-  # mutate(Average.Adjusted.Area = mean(Adjusted.Area, na.rm = TRUE)) %>%
-  # select(Mass.Feature, SampID, Average.Adjusted.Area) %>%
-  select(Mass.Feature, Replicate.Name, SampID, Area.BMISd.Normd) %>%
+  select(Mass.Feature, Replicate.Name, SampID, Area.BMISd.Normd, Adjusted.Area) %>%
   unique()
-AnovaB12 <- AnovaB12[complete.cases(AnovaB12), ]
-
-# WAnovaB12 <- AnovaB12 %>%
-#   pivot_wider(names_from = Mass.Feature,
-#               values_from = Adjusted.Area)
+full.BMISd <- AnovaB12[complete.cases(AnovaB12), ]
 
 
-## my version of online test
-test_df <- AnovaB12 %>%
+# Set up data for ANOVA
+AnovaB12 <- full.BMISd %>%
   select(-Replicate.Name) %>%
   mutate(SampID = factor(SampID, ordered = TRUE)) %>%
   arrange(Mass.Feature)
-glimpse(test_df)
-levels(test_df$SampID)
+glimpse(AnovaB12) #Use for the future
+levels(AnovaB12$SampID)
 
-summary_test_df <- test_df %>%
-  group_by(SampID, Mass.Feature) %>%
-  summarise(
-    count_SampID = n(),
-    mean_area = mean(Area.BMISd.Normd, na.rm = TRUE),
-    sd_area = sd(Area.BMISd.Normd, na.rm = TRUE)
-  )
-
-ggplot(test_df, aes(x = SampID, y = Area.BMISd.Normd, fill = SampID)) +
+# Graph normalized areas for reference
+ggplot(AnovaB12, aes(x = SampID, y = Area.BMISd.Normd, fill = SampID)) +
   geom_boxplot() +
   facet_wrap(~Mass.Feature) +
-  theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5)) +
   geom_jitter(shape = 15,
               color = "steelblue",
               position = position_jitter(0.21))
 
-# split and apply function, result is a named list:
-mylist <- lapply(split(test_df, test_df$Mass.Feature), function(i) {
+# Split and apply ANOVA function, result is a named list:
+AnovaList <- lapply(split(AnovaB12, AnovaB12$Mass.Feature), function(i) {
   anova(lm(Area.BMISd.Normd ~ SampID, data = i))
 })
 
-names(mylist)
-mylist[[1]][["Pr(>F)"]]
+# Turn list to dataframe
+AnovaDF <- do.call(rbind, lapply(AnovaList, function(x) {temp <- unlist(x)}))
+AnovaDF <- as.data.frame(AnovaDF)
+colnames(AnovaDF)[9] <- "Pvalue"
+
+AnovaDF <- AnovaDF %>%
+  rownames_to_column(var = "Mass.Feature") %>%
+  select(Mass.Feature, Pvalue) %>%
+  mutate(Significant = ifelse(Pvalue < 0.1, TRUE, FALSE))
+
+# Join with original data to plot
+toPlot <- full.BMISd %>%
+  left_join(AnovaDF) %>%
+  group_by(Mass.Feature, SampID) %>%
+  mutate(AveSmp = mean(Area.BMISd.Normd, na.rm = TRUE)) %>%
+  select(Mass.Feature, SampID, AveSmp, Pvalue, Significant) %>%
+  unique()
+
+a <- ggplot(toPlot, aes(x = AveSmp, y = Pvalue, fill = Significant,
+                          label = Mass.Feature)) +
+  geom_point(size = 3, shape = 21, stroke=0) +  
+  scale_fill_manual(values = c("grey", "royalblue4")) +
+  scale_alpha_manual(values = c(1, 0.5)) +
+  ggtitle("TEST") +
+  theme(plot.title = element_text(size = 15),
+        legend.position="none",
+        axis.title.y=element_text(size=9),
+        axis.title.x=element_text(size=9),
+        axis.text=element_text(size=9)) +
+  labs(x="Average peak size") +
+  labs(x="Average peak size", y=expression(paste(Log[2], "T0/DSW", sep = ""))) +
+  theme(legend.position="right") 
+  #geom_text(data = toPlot)
+a
+
 
 
 test_anova_one_way <- aov(Area.BMISd.Normd~SampID, data = test_df)
 summary(test_anova_one_way)
 summary(test_anova_one_way)[[1]][["Pr(>F)"]]
-TukeyHSD(test_anova_one_way)
-
-
-df <- do.call(rbind,             # rbind list of data.frames output by lapply
-        lapply(mylist, # loop through list, first drop outer names
-               function(x) { 
-                 temp <- unlist(x)}))
-
-df <- as.data.frame(df)
+test <- TukeyHSD(test_anova_one_way)
 
 # two way isn't useful because mass feature isn't grouping?
 # test_anova_two_way <- aov(Area.BMISd.Normd~SampID + Mass.Feature, data = test_df)
